19e6e79639cfff08142440b7cf690b351cfb44f7
IVB107 <ianvb107@gmail.com>
2019-03-18 17:39:53 -0500

    Completed GET request to get user by id

--- index.js
+++ index.js
@@ -36,7 +36,20 @@ server.get('/api/users', (req, res) => {
         })
 })
 
-// server.get()
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The user information could not be retrieved.' });
+        })
+    // ? res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+    // : res.
+
+})
 
 // server.delete()
 
