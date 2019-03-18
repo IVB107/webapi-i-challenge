41904e5365a5dbd3b3dfb0016177ee045fa1c27c
IVB107 <ianvb107@gmail.com>
2019-03-18 17:19:35 -0500

    Completed GET request to retrieve users array

--- index.js
+++ index.js
@@ -26,7 +26,15 @@ server.post('/api/users', (req, res) => {
         })
 })
 
-// server.get()
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The users information could not be retrieved.' });
+        })
+})
 
 // server.get()
 
