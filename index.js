83538a120375d54d9fdba3ff38ecfc9c2f3eb774
IVB107 <ianvb107@gmail.com>
2019-03-18 17:12:28 -0500

    Completed POST request

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db');
+
+server.use(express.json());
+
+server.get('/api', (req, res) => {
+    res.send('Welcome to the Users API!');
+})
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    (!userInfo.name || !userInfo.bio)
+    ? res.status(404).json({ errorMessage: 'Please provide name and bio for the user.' })
+    : db
+        .insert(userInfo)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'There was an error while saving the user to the database' });
+        })
+})
+
+// server.get()
+
+// server.get()
+
+// server.delete()
+
+// server.put()
+
+server.listen(4000, () => {
+    console.log('Listening on Port 4000...');
+})
