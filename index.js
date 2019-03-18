f44c3341dc1808679a41577815000e1e4ca3b888
IVB107 <ianvb107@gmail.com>
2019-03-18 18:17:11 -0500

    Completed PUT request, updated user by id

--- index.js
+++ index.js
@@ -66,7 +66,26 @@ server.delete('/api/users/:id', (req, res) => {
         })
 })
 
-// server.put()
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
+
+    if (!user.name || !user.bio) {
+        return res.status(400).json({ message: 'Please provide name and bio for the user.' });
+    }
+
+    db.update(id, user)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json(updated);
+            } else {
+                res.status(404).json({ message: 'The user with the specified ID does not exist. '});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The user information could not be modified.' });
+        })
+})
 
 server.listen(4000, () => {
     console.log('Listening on Port 4000...');
